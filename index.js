6eed1e3cb3eb6ea5660e416b49e6ececbeeced4e
korede <egbeolowoakorede97@gmail.com>
2021-06-18 15:30:09 +0800

    Making Changes

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ require('./config/passport')(passport);
 
 // Connecting to MongoDB...
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/student-mgmt-sys', {
+mongoose.connect("mongodb+srv://schl-info-system:school@cluster0.morsc.mongodb.net/schl-info-system?retryWrites=true&w=majority", {
     useNewUrlParser: true
 }).then(() => console.log('Connected to MongoDB Server...')).catch(err => console.error('Error occured connecting to MongoDB...', err));
 
